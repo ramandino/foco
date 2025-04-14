f574cfb1de28fe20b14f956ba18913febf2b254e
Ramiro Andino <DELL@Ramiro.local>
2025-04-14 12:25:11 -0500

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1 class="">que onda</h1>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// Standard Tailwind configuration
+export const content = [
+  "./src/**/*.{js,jsx,ts,tsx}",
+  "./index.html",
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
+
